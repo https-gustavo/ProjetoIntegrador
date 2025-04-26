2356a52fe4f65196e9de91a161bc6503fba71228
https-gustavo <gustavopastredemenezes@gmail.com>
2025-04-25 22:04:44 -0300

    Atualiza .gitignore, adiciona Procfile e requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -110,7 +110,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://https-gustavo.github.io"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
